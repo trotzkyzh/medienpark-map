a0f9020ba49e129fd72cf3d0a652e423d5bd1009
trotzkyzh <52784097+trotzkyzh@users.noreply.github.com>
2021-02-12 05:43:47 +0000

    Deploying to gh-pages from master @ fc1764d1e883a0cb97174714df6ede9473ae8d5c 🚀

---
+++ Pipoya RPG Tileset 32x32/SampleMap/[Base]BaseChip_pipo.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="[Base]BaseChip_pipo" tilewidth="32" tileheight="32" tilecount="1064" columns="8">
+ <image source="[Base]BaseChip_pipo.png" width="256" height="4256"/>
+ <tile id="328">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="330">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="336">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="338">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="344">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="346">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
